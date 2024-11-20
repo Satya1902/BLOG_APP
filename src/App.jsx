4bf3e7f3634e34de549ee6d3bd23fb852575e1f2
satya1902 <er.satyagautam@gmail.com>
2024-11-20 09:24:20 +0530

    first commit to save frontend

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="w-[100vw] h-[100vh] bg-gray-900">
+
+       <div className="w-full h-full flex justify-center ">
+          <h1 className="text-red-400 text-3xl text-bold mt-12">BLOG APP</h1>
+       </div>
+
+    </div>
+  );
+}
+
+export default App;
